cae5cf399a8fad1168dd547d60d3d1dce1af6744
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2022-10-21 23:43:09 -0400

    idek anymore

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{js,jsx,ts,tsx}"],
+  theme: {
+
+    colors: {
+      'darkBrown': '#A57548',
+      'lightBrown': '#F6C28B',
+      'sandy': '#FCD7AD',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
